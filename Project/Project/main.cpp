b484af77b21d748153570d96f8b6b9b852177c7f
Arun Godwin Patel <arungodwinpatel@googlemail.com>
2019-11-10 19:10:08 +0000

    feat: adding Project main.cpp files and executable

---
+++ Project/Project/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  Project
+//
+//  Created by Arun Godwin Patel on 21/10/2019.
+//  Copyright © 2019 Arun Godwin Patel. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+// creating projects, something that can store many files
+// header is the library file
+
+int main() {
+    
+    cout << "Hello world... " << endl;
+    return 0;
+    
+}
